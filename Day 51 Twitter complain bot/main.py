2ad74c04ce3c8d59ba6278c96f599216eaf48e2c
Amatya Sawant <amatyasawant2022@vitbhopal.ac.in>
2025-08-27 18:25:40 +0530

    Add files via upload

---
+++ Day 51 Twitter complain bot/main.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+PROMISED_DOWN = 150
+PROMISED_UP = 10
+TWITTER_EMAIL = "amatsaw2004@gmail.com"
+Username = "@Amat1082568"
+TWITTER_PASSWORD = "Amatya@2004"
+
+
+class InternetSpeedTwitterBot:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.up = 0
+        self.down = 0
+
+    def get_internet_speed(self):
+        self.driver.get("https://www.speedtest.net/")
+
+        # Depending on your location, you might need to accept the GDPR pop-up.
+        # accept_button = self.driver.find_element(By.ID, value="_evidon-banner-acceptbutton")
+        # accept_button.click()
+
+        time.sleep(3)
+
+        go_button = self.driver.find_element(By.CSS_SELECTOR, value=".start-button a")
+        go_button.click()
+
+        time.sleep(60)
+        self.up = self.driver.find_element(By.XPATH, value='//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[2]/div/div[4]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        self.down = self.driver.find_element(By.XPATH, value='//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[2]/div/div[4]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
+
+
+    def tweet_at_provider(self):
+        self.driver.get("https://x.com/i/flow/login")
+
+        time.sleep(2)
+        time.sleep(3)
+        email = self.driver.find_element(By.CSS_SELECTOR, 'input[autocomplete="username"]')
+        email.send_keys(TWITTER_EMAIL)
+        email.send_keys(Keys.ENTER)
+
+        time.sleep(4)
+        username = self.driver.find_element(By.XPATH, value='//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input')
+        username.send_keys(Username)
+        username.send_keys(Keys.ENTER)
+        time.sleep(5)
+        password = self.driver.find_element(By.CSS_SELECTOR, value='input[autocomplete="current-password')
+        password.send_keys(TWITTER_PASSWORD)
+        password.send_keys(Keys.ENTER)
+        time.sleep(10)
+        tweet_compose = self.driver.find_element(By.CSS_SELECTOR, 'div[data-testid="tweetTextarea_0"]')
+
+        tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+        tweet_compose.send_keys(tweet)
+        time.sleep(3)
+
+        tweet_button = self.driver.find_element(By.CSS_SELECTOR, 'button[data-testid="tweetButtonInline"]')
+        tweet_button.click()
+
+        time.sleep(2)
+        self.driver.quit()
+
+
+bot = InternetSpeedTwitterBot()
+bot.get_internet_speed()
+bot.tweet_at_provider()
